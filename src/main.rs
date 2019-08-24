a801a560e6cbfe99c69fda16b7f848c3d6f95be6
Jan Szumiec <jan.szumiec@gmail.com>
2019-08-24 12:15:02 +0200

    Sort of working calculator.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
 use std::io::Read;
+use std::convert::TryInto;
 
 struct KeyboardInput {
 }
 
 #[derive(Debug)]
 enum Key {
+    NoOp,
     Digit(u8),
     Add,
     Subtract,
@@ -21,24 +23,88 @@ impl Iterator for KeyboardInput {
     fn next(&mut self) -> Option<Key> {
         let mut buf: [u8; 1] = [0];
         if let Ok(()) = io::stdin().read_exact(&mut buf) {
-            return match buf[0] {
-                0x2b => Some(Key::Add),
-                0x2d => Some(Key::Subtract),
-                0x2a => Some(Key::Multiply),
-                0x2f => Some(Key::Divide),
-                0x10 => Some(Key::Enter),
-                99 => Some(Key::Clear),
-                x @ 48..=59 => Some(Key::Digit(x - 48)),
-                _ => None
+            let c: char = char::from(buf[0]);
+            return match c {
+                '+' => Some(Key::Add),
+                '-' => Some(Key::Subtract),
+                '*' => Some(Key::Multiply),
+                '/' => Some(Key::Divide),
+                '\n' => Some(Key::Enter),
+                'c' => Some(Key::Clear),
+                '0'..='9' => {
+                    let digit: u8 = c.to_digit(10).unwrap().try_into().unwrap();
+                    Some(Key::Digit(digit))
+                },
+                _ => {
+                    Some(Key::NoOp)
+                }
             }
         }
+        // ^D ends up here
         None
+    }
+}
+
+#[derive(Debug)]
+struct Calculator {
+    stack: Vec<i32>,
+}
+
+impl Calculator {
+    fn new() -> Calculator {
+        Calculator {
+            stack: vec![0],
+        }
+    }
+    
+    fn append_digit(&mut self, d: u8) {
+        let mut digits;
+        match self.stack.pop() {
+            Some(n) => {
+                digits = n.to_string(); 
+            },
+            None => {
+                digits = "0".to_string();
+            }
+        }
+        digits.push_str(&d.to_string());
+        let number = digits.parse::<i32>().unwrap();
+        self.stack.push(number);
+    }
+
+    fn enter(&mut self) {
+        if let Some(i) = self.stack.last() {
+            if i.ne(&0) {
+                self.stack.push(0);
+            }
+        }
+    }
+
+    fn add(&mut self) {
+        if self.stack.len() >= 2 {
+            let a = self.stack.pop().unwrap();
+            let b = self.stack.pop().unwrap();
+            self.stack.push(a+b);
+        }
     }
 }
 
 fn main() {
     let k = KeyboardInput{};
+    let mut c = Calculator::new();
     for b in k {
-        println!("{:?}", b)
+        println!("{:?}", c);
+        match b {
+            Key::Digit(d) => {
+                c.append_digit(d);
+            }
+            Key::Enter => {
+                c.enter();
+            }
+            Key::Add => {
+                c.add();
+            }
+            _ => {}
+        }
     }
 }
