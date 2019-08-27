260af67accbf43059dd51bbfbffc6b7388215959
Jan Szumiec <jan.szumiec@gmail.com>
2019-08-27 09:07:46 +0200

    Moved stack into calc, return a copy of stack for read-only inspection.

---
+++ src/calc.rs
@@ -0,0 +1,28 @@
+pub struct Calculator {
+    stack: Vec<i32>
+}
+
+impl Calculator {
+    pub fn new() -> Self {
+        let v: Vec<i32> = vec![0];
+        Self {
+            stack: v
+        }
+    }
+
+    pub fn stack(&self) -> Vec<i32> {
+        self.stack.clone()
+    }
+
+    pub fn digit(&mut self, d: u8) {
+        
+    }
+
+    pub fn add(&mut self) {
+        if self.stack.len() >= 2 {
+            let a = self.stack.pop().unwrap();
+            let b = self.stack.pop().unwrap();
+            self.stack.push(a+b);
+        }
+    }
+}
--- src/ui.rs
+++ src/ui.rs
@@ -1,35 +1,37 @@
 use std::convert::TryInto;
 use ncurses as n;
+use crate::calc::Calculator;
 
-pub struct UI<'a> {
+pub struct UI {
     stack_win: n::WINDOW,
-    stack: &'a Vec<i32>
+    calc: Calculator
 }
 
-const WindowHeight: i32 = 20;
-const WindowLength: i32 = 20;
+const WINDOW_HEIGHT: i32 = 20;
+const WINDOW_LENGTH: i32 = 20;
 
-impl<'a> UI<'a> {
-    pub fn new(stack: &'a Vec<i32>) -> Self {
+impl UI {
+    pub fn new() -> Self {
         n::initscr();
         n::raw();
         n::noecho();
         Self {
-            stack_win: n::newwin(WindowHeight, WindowLength, 0, 0),
-            stack: stack
+            stack_win: n::newwin(WINDOW_HEIGHT, WINDOW_LENGTH, 0, 0),
+            calc: Calculator::new()
         }
     }
 
     fn draw(&self) {
+        let stack = self.calc.stack();
         n::box_(self.stack_win, 0, 0);
-        let len32: i32 = self.stack.len().try_into().unwrap();
-        let miny: i32 = WindowHeight - len32 - 1;
+        let len32: i32 = stack.len().try_into().unwrap();
+        let miny: i32 = WINDOW_HEIGHT - len32 - 1;
         let x = 1;
 
         let mut stack_idx = 0;
-        for y in miny..(WindowHeight - 1) {
+        for y in miny..(WINDOW_HEIGHT - 1) {
             // TODO: right-justify formatting here
-            n::mvwprintw(self.stack_win, y, x, &self.stack[stack_idx].to_string());
+            n::mvwprintw(self.stack_win, y, x, &stack[stack_idx].to_string());
             stack_idx += 1;
         }
         
@@ -37,44 +39,22 @@ impl<'a> UI<'a> {
         n::wrefresh(self.stack_win);
     }
     
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         self.draw();
-        n::getch();
-    }
-}
-
-impl<'a> Drop for UI<'a> {
-    fn drop(&mut self) {
-        n::endwin();
-    }
-}
-
-struct EventIterator {
-    digits: String
-}
-
-enum Event {
-    StoreNumber,
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Clear
-}
-
-impl EventIterator {
-    fn new() -> Self {
-        Self {
-            digits: String::new()
+        let c = n::getch();
+        match c {
+            48..=58 => {
+                let d: u8 = (c - 48).try_into().unwrap();
+                self.calc.digit(d);
+            }
+            _ => {
+            }
         }
     }
 }
 
-// impl Iterator for EventIterator {
-//     type Item = Event;
-
-//     fn next(&mut self) -> Option<Event> {
-//         let c = n::getch();
-        
-//     }
-// }
+impl Drop for UI {
+    fn drop(&mut self) {
+        n::endwin();
+    }
+}
